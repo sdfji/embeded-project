708ce7c1630810d845704896e0b73a43686668ce
김지철 <shopopl@naver.com>
2017-06-01 12:24:57 +0900

    HeartBeat
    HeartBeatTest

--- sensors/band.js
+++ sensors/band.js
@@ -6,15 +6,21 @@ let modeState = 'off';
 let prevValue = 0;
 let Count = 0;
 let tossAndTurnData = 0;
+let oldValue = 0;
+let alpha = 0.75; 
+let period = 100; //측정 주기.
+
 
 const gpio = require('wiring-pi')
 const BALLTILT = 29
 const TOUCHED = 28
+const HEARTBEAT = 25
 //const util = require()
 
 gpio.wiringPiSetup()
 gpio.pinMode(BALLTILT, gpio.INPUT)
 gpio.pinMode(TOUCHED, gpio.INPUT)
+gpio.pinMode(HEARTBEAT, gpio.INPUT)
 /*setInterval(() => {
     const data = gpio.digitalRead(BALLTILT)
 console.log(data)
@@ -23,6 +29,7 @@ console.log(data)
  setInterval(TossAndTurn, 500)
  setInterval(SecurityRequest, 100)
  setInterval(SetServer, 1000)
+ setInterval(HeartBeatInfo, 100)
 
 function TossAndTurn() {
     if(modeState == 'off')
@@ -56,6 +63,16 @@ function SecurityRequest() {
 	}
     }
     
+}
+
+function HeartBeatInfo()
+{
+    const rawValue = gpio.analogRead(HEARTBEAT)
+    let value = alpha * oldValue + (1 - alpha) * rawValue;
+
+    console.log(rawValue)
+    console.log('VALUE : ' + value);
+    oldValue = rawValue
 }
 
 function SetServer() {
